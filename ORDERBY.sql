a6806b322545a684f616111aa4cc50af6011d67d
moon9109 <hanhannetnet@naver.com>
2021-03-22 20:52:56 +0900

    Add SQL file [ORDERBY.sql]

---
+++ ORDERBY.sql
@@ -0,0 +1,32 @@
+select 
+
+	FIRST_NAME
+,   LAST_NAME
+from 
+	customer 
+order by FIRST_NAME asc 
+;
+
+
+
+select 
+	FIRST_NAME
+,   LAST_NAME
+
+from 
+	customer 
+order by FIRST_NAME desc 
+;
+
+
+select 
+	FIRST_NAME --ASC 
+,   LAST_NAME --DESC
+
+from 
+	customer 
+order by FIRST_NAME asc 
+	,    LAST_NAME desc 
+	;
+
+
